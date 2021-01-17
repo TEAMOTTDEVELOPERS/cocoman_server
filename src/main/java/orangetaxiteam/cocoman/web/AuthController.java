aca7a784b736d6535e1aa2999df2e68a47101060
GunHee Jang <30435522+GunHee5719@users.noreply.github.com>
2021-01-17 15:29:34 +0900

    feat: setting for swagger (#15)

--- src/main/java/orangetaxiteam/cocoman/web/AuthController.java
+++ src/main/java/orangetaxiteam/cocoman/web/AuthController.java
@@ -1,4 +1,17 @@
 package orangetaxiteam.cocoman.web;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class AuthController {
+    @RequestMapping(value = "/sample", method = RequestMethod.GET)
+    @ApiOperation(value = "Sample api for this app", tags = "Sample!")
+    public String test(){
+        return "Hello";
+    }
+
 }
