66234c08920fbc35d192b4020f8e940858156fdb
ParkHaeSung <hea9549.github@gmail.com>
2021-01-17 14:55:21 +0900

    feat: skel of models

--- src/main/java/orangetaxiteam/cocoman/domain/User.java
+++ src/main/java/orangetaxiteam/cocoman/domain/User.java
@@ -6,13 +6,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     @Id
+    @GeneratedValue
     private String id;
 }
