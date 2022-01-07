bc6140432298cc923addc388aed934040130c1b3
Jinyoung Park <jiny981004@gmail.com>
2022-01-07 22:18:20 +0900

    Update UserController.java

--- src/main/java/orangetaxiteam/cocoman/web/UserController.java
+++ src/main/java/orangetaxiteam/cocoman/web/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
