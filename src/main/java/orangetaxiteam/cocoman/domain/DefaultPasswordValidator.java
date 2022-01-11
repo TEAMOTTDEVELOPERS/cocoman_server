e5486df6a3a06028268463bb0c1aacc809a94901
Jinyoung Park <jiny981004@gmail.com>
2022-01-11 21:31:46 +0900

    Revert "feat: validate password for login (#111)"
    This reverts commit df79a610ac938beb5f1b9df61d347d595648eedd.

--- src/main/java/orangetaxiteam/cocoman/domain/DefaultPasswordValidator.java
+++ src/main/java/orangetaxiteam/cocoman/domain/DefaultPasswordValidator.java
@@ -1,20 +1,11 @@
 package orangetaxiteam.cocoman.domain;
 
-import orangetaxiteam.cocoman.domain.exceptions.BadRequestException;
-import orangetaxiteam.cocoman.domain.exceptions.ErrorCode;
-import orangetaxiteam.cocoman.infra.DefaultPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DefaultPasswordValidator implements PasswordValidator {
-    private DefaultPasswordEncoder defaultPasswordEncoder = new DefaultPasswordEncoder();
-
     @Override
     public void validate(User user, String password) {
-        if (!defaultPasswordEncoder.matches(password,user.getPassword())) {
-            throw new BadRequestException(
-                    ErrorCode.SIGNIN_PASSWORD_DOES_NOT_MATCH,
-                    "Password does not match");
-        }
+        // TODO: implement me
     }
 }
